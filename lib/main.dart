049a5b7805ac1e03d5aa37201a82a71313c093c7
unknown <ishwamgarg19@gmail.com>
2020-08-24 16:19:14 +0530

    Skills ADDED

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +9,8 @@ void main() {
     debugShowCheckedModeBanner: false,
     theme: new ThemeData(
       brightness: Brightness.dark,
-      primarySwatch: Colors.amber,
-      accentColor: Colors.amberAccent,
+      primarySwatch: Colors.indigo,
+      accentColor: Colors.indigoAccent,
     ),
   ));
 }
@@ -19,7 +21,31 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+
+  AnimationController _pageAnimationController;  // for page animation
+  Animation<double> _pageAnimation;              // for page animation
+
+  @override
+  void initstate()
+  {
+    super.initState();
+    _pageAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 500),
+    );  // instances
+    _pageAnimation = new CurvedAnimation(
+        parent: _pageAnimationController,
+        curve: Curves.easeIn,
+    );
+
+    _pageAnimation.addListener(() {
+      this.setState(() {
+       _pageAnimationController.forward();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +59,13 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.only(top:50.0,left: 30.0,right: 30.0,bottom: 30.0),
             child:  new Text("My Portfolio",style: new TextStyle(
               decoration: TextDecoration.underline,
-              fontSize: 34.0,
+              fontSize: 34,
               color: Colors.white,
               fontWeight: FontWeight.bold
             ),),
             ),
-            CircleAvatar(radius: 100.0,backgroundImage: AssetImage("Images/preparation.jpg"), ), //will pass my image here
+            CircleAvatar(minRadius: 20.0,backgroundImage: AssetImage("Images/preparation.jpg"),maxRadius: 100,
+            ), //will pass my image here
             new Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.only(top:30.0,left: 30.0,right: 30.0),
@@ -73,7 +100,7 @@ class _HomeState extends State<Home> {
                         color: Color(0xff187bcd), //blue Color
                       ),),
                     ),
-                    new Container(
+                     new Container(
                       alignment: Alignment.center,
                       padding: const EdgeInsets.only(top:0.0,left: 15.0,right: 15.0,bottom:15),
                       child: new Text("I am a 2nd year CSE Student. I have passed my 12th in PCM with 92% in year 2020 from DPSG MEERUT ROAD.", style: new TextStyle(
@@ -81,22 +108,23 @@ class _HomeState extends State<Home> {
                         fontWeight: FontWeight.w900,
                         color: Colors.white,
                       ),),
-                    ),
-                    new Container(
+                    ), // description
+
+                      new Container(
                       alignment: Alignment.center,
                       padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
                       child:
                       Row(children: <Widget>[
-                        Icon(Icons.school,color: Colors.white,size: 30,),
+                        Icon(Icons.school,color: Colors.white70 ,size: 30,),
                         Text(" AKGEC GHAZIABAD ",style: TextStyle(color: Colors.white,fontSize: 20,),),
                       ]),
-                    ), //Colleg
+                    ), //College
                   new Container(
                     alignment: Alignment.center,
                     padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
                     child: 
                     Row(children: <Widget>[
-                    Icon(Icons.email,color: Colors.white,size: 30,),
+                    Icon(Icons.email,color: Colors.white70,size: 30,),
                     Text(" ishwamgarg19@gmail.com ",style: TextStyle(color: Colors.white,fontSize: 20,),),
                     ]),
                   ),//email
@@ -105,7 +133,7 @@ class _HomeState extends State<Home> {
                       padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
                       child:
                     Row(children: <Widget>[
-                    Icon(Icons.phone,color: Colors.white,size: 30,),
+                    Icon(Icons.phone,color: Colors.white70,size: 30,),
                     Text(" +91-8851850004",style: TextStyle(color: Colors.white,fontSize: 20,),),
                     ]),
                     ),// phone
@@ -114,7 +142,7 @@ class _HomeState extends State<Home> {
                       padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
                       child:
                   Row(children: <Widget>[
-                    Icon(Icons.location_on,color: Colors.white,size: 30,),
+                    Icon(Icons.location_on,color: Colors.white70,size: 30,),
                     Text(" India ",style: TextStyle(color: Colors.white,fontSize: 20,),),
                   ]),
                   ),
@@ -123,17 +151,91 @@ class _HomeState extends State<Home> {
                   padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
                   child:
                   Row(children: <Widget>[
-                    Icon(Icons.access_time,color: Colors.white,size: 30,),
+                    Icon(Icons.access_time,color: Colors.white70,size: 30,),
                     Text(" 5pm to 11pm ",style: TextStyle(color: Colors.white,fontSize: 20,),),
                   ]),
                 ),
                     new Container(
                       padding: const EdgeInsets.only(top:0.0,left: 10,right: 10,bottom: 10),
                       alignment: Alignment.topLeft,
-                      child: new Text("SOCIAL",style: TextStyle(
-                        fontSize: 60,
+                      child: new Text("SKILLS",style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 40,
                         fontWeight: FontWeight.bold,
                         color: Color(0xff187bcd), //blue Color
+                      ),),
+                    ),//skills
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.code,color: Colors.white70 ,size: 30,),
+                        Text(" C++ ",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ), //C++
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.code,color: Colors.white70,size: 30,),
+                        Text(" Python ",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ),//Python
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.code,color: Colors.white70,size: 30,),
+                        Text(" HTML ",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ),// HTML
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.code,color: Colors.white70,size: 30,),
+                        Text(" CSS ",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ),//CSS
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.android,color: Colors.white70,size: 30,),
+                        Text(" Android Development",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ),//Android dev
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:10.0,bottom: 10.0,left: 5,right: 5),
+                      child:
+                      Row(children: <Widget>[
+                        Icon(Icons.android,color: Colors.white70,size: 30,), // add flutter icon
+                        Text(" FLUTTER ",style: TextStyle(color: Colors.white,fontSize: 20,),),
+                      ]),
+                    ),//Flutter
+                    new Container(
+                      padding: const EdgeInsets.only(top:0.0,left: 10,right: 10,bottom: 10),
+                      alignment: Alignment.topLeft,
+                      child: new Text("SOCIAL",style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff187bcd), //blue Color
+                      ),),
+                    ), //Social
+                    new Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(top:0.0,left: 15.0,right: 15.0,bottom:15),
+                      child: new Text("I Am Available On Instagram,Facebook,Github. Usernames are Below: ", style: new TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
                       ),),
                     ),
                 ],)
